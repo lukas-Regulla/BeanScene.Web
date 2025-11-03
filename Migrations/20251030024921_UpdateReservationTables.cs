e8fe79989cb0e76de98ac1009ef792fc7abd581d
lukas regulla <lukas.regulla@studytafensw.edu.au>
2025-11-03 23:53:46 +1100

    Add project files.

---
+++ Migrations/20251030024921_UpdateReservationTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeanScene.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReservationTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/SittingSchedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeanScene.Web.Models;
+
+public partial class SittingSchedule
+{
+    public int SittingScheduleId { get; set; }
+
+    public string Stype { get; set; } = null!;
+
+    public DateTime StartDateTime { get; set; }
+
+    public DateTime EndDateTime { get; set; }
+
+    public int Scapacity { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public bool IsClosed { get; set; }
+
+    public virtual ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
